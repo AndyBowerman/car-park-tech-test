882ba3723651439b45f1a1b5ffc87221206c85aa
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-10 09:31:11 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/*
+small - motorbike
+medium - cars, motorbikes or 3 for a van
+large - 
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
